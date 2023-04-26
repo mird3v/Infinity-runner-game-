99e3526297b30439c377cd8878788f4e5abed4f0
Miriam Hamzaoui <mimi.d3v@gmail.com>
2023-04-26 14:09:45 +0200

    still not done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const gameContainer = document.getElementById("game-container");
 const character = document.getElementById("character");
+const playButton = document.getElementById("play-button");
 
 let isJumping = false;
 let obstaclesInterval;
 let score = 0;
+let obstacles = [];
+let frames = 0;
 
 function jump() {
   if (isJumping) return;
@@ -11,7 +14,7 @@ function jump() {
   let position = 0;
 
   const jumpInterval = setInterval(() => {
-    if (position >= 100) {
+    if (position >= 200) {
       clearInterval(jumpInterval);
       const fallInterval = setInterval(() => {
         if (position <= 0) {
@@ -33,13 +36,17 @@ function createObstacle() {
   const obstacleElem = document.createElement("div");
   obstacleElem.classList.add("obstacle");
   gameContainer.appendChild(obstacleElem);
-  obstacleElem.style.right = "700px";
+  obstacleElem.style.right = "0px";
 
   const moveObstacle = setInterval(() => {
     const playerPosition = parseInt(character.style.bottom);
     const obstaclePosition = parseInt(obstacleElem.style.right);
-
-    if (obstaclePosition > 0 && obstaclePosition < 50 && playerPosition < 50) {
+    console.log(obstaclePosition);
+    if (
+      obstaclePosition > 0 &&
+      obstaclePosition < 50 &&
+      detectCollision(character, obstacleElem)
+    ) {
       clearInterval(moveObstacle);
       clearInterval(obstaclesInterval);
       alert("Game Over");
@@ -48,13 +55,65 @@ function createObstacle() {
       gameContainer.removeChild(obstacleElem);
       document.getElementById("score").innerText = `Score: ${++score}`;
     } else {
-      obstacleElem.style.right = `${obstaclePosition - 10}px`;
+      obstacleElem.style.right = `${obstaclePosition + 10}px`;
     }
   }, 100);
 }
 
+function detectCollision(player, obstacle) {
+  let playerBounding = player.getBoundingClientRect();
+  let obstacleBounding = obstacle.getBoundingClientRect();
+
+  return (
+    playerBounding.bottom > obstacleBounding.top &&
+    playerBounding.top < obstacleBounding.bottom &&
+    playerBounding.right > obstacleBounding.left &&
+    playerBounding.left < obstacleBounding.right
+  );
+}
+
+function launchGame() {
+  frames++;
+  if (frames % 120 === 0) {
+    const obstacleElem = document.createElement("div");
+    obstacleElem.classList.add("obstacle");
+    gameContainer.appendChild(obstacleElem);
+    obstacleElem.style.right = "0px";
+    obstacles.push(obstacleElem);
+  }
+
+  const playerBounding = character.getBoundingClientRect();
+
+  for (const obs of obstacles) {
+    const obstaclePosition = parseInt(obs.style.right);
+    const obstacleBounding = obs.getBoundingClientRect();
+    if (
+      obstacleBounding.left < playerBounding.right &&
+      obstacleBounding.right > playerBounding.left &&
+      playerBounding.bottom >= obstacleBounding.top
+    ) {
+      clearInterval(obstaclesInterval);
+      alert("Game Over");
+    } else if (obstacleBounding.right < playerBounding.left && !obs.scored) {
+      obs.scored = true;
+      document.getElementById("score").innerText = `Score: ${++score}`;
+    } else if (obstaclePosition <= -50) {
+      obs.remove();
+      obstacles.splice(obstacles.indexOf(obs), 1);
+    } else {
+      obs.style.right = `${obstaclePosition + 10}px`;
+    }
+  }
+}
+
 function startGame() {
-  obstaclesInterval = setInterval(createObstacle, 2000);
+  //obstaclesInterval = setInterval(createObstacle, 2000);
+  obstaclesInterval = setInterval(launchGame, 1000 / 60);
+}
+
+function handlePlayButtonClick() {
+  playButton.style.display = "none";
+  startGame();
 }
 
 document.addEventListener("keydown", (e) => {
@@ -63,4 +122,4 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-startGame();
+playButton.addEventListener("click", handlePlayButtonClick);
